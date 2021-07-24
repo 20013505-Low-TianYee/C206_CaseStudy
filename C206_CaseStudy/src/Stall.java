9fb2d22b9156e4c7e8799d3375fc160638ba4525
20028903-Bakman-Axel-Umar <20028903@myrp.edu.sg>
2021-07-24 11:30:54 +0800

    Created stall class

---
+++ C206_CaseStudy/src/Stall.java
@@ -0,0 +1,24 @@
+
+public class Stall {
+	private int stallNo;
+	private String storeName;
+	private String operationDate;
+	
+	public Stall(int stallNo, String storeName, String operationDate) {
+		this.stallNo = stallNo;
+		this.storeName = storeName;
+		this.operationDate = operationDate;
+	}
+
+	public int getStallNo() {
+		return stallNo;
+	}
+
+	public String getStoreName() {
+		return storeName;
+	}
+
+	public String getOperationDate() {
+		return operationDate;
+	}
+}
