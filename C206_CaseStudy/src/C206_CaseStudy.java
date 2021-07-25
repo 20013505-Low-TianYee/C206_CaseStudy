aaaca0e745c8c3060bee44a716c3f8beca14f5cc
19020947 <19020947@DESKTOP-L990C7H>
2021-07-25 19:28:32 +0800

    Add menu (partial)

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -74,7 +74,17 @@ public class C206_CaseStudy {
 		System.out.println("2. View stalls");
 		System.out.println("3. Delete stall");
 		System.out.println("4. Update owner of stall");
-
+		System.out.println("5. ");
+		System.out.println("6. ");
+		System.out.println("7. ");
+		System.out.println("8. ");
+		System.out.println("9. ");
+		System.out.println("10. ");
+		System.out.println("11. ");
+		System.out.println("12. ");
+		System.out.println("13. ");
+		System.out.println("14. ");
+		System.out.println("15. ");
 
 	}
 	//START OF STALL METHODS===========================================================================================
